cfabd977d113fbd3a35e9ba9b177f56b1e18ca5b
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2019-02-18 22:47:51 +0900

    9. 스프링 mvc 빈설정

--- src/main/java/me/anpan/WebConfig.java
+++ src/main/java/me/anpan/WebConfig.java
@@ -3,12 +3,34 @@ package me.anpan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan
 public class WebConfig {
+
+    @Bean
+    public HandlerMapping handlerMapping() {
+        RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
+        //추가적인 set 가능
+        handlerMapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return handlerMapping;
+    }
+
+    @Bean
+    public HandlerAdapter handlerAdapter() {
+        RequestMappingHandlerAdapter handlerAdapter = new RequestMappingHandlerAdapter();
+        //추가적인 set 가능
+        handlerAdapter.setAsyncRequestTimeout(100L);
+        return handlerAdapter;
+    }
 
     @Bean
     public ViewResolver viewResolver() {
