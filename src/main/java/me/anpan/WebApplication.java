3f2f7ca9207f61637e2e83a1bb84a86f79af97f2
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2019-02-18 22:33:45 +0900

    8. mvc동작 마무리 (web.xml삭제)

---
+++ src/main/java/me/anpan/WebApplication.java
@@ -0,0 +1,22 @@
+package me.anpan;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebApplication implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);
+        context.refresh();
+
+        DispatcherServlet servlet = new DispatcherServlet(context);
+        ServletRegistration.Dynamic app = servletContext.addServlet("DispatcherServlet",servlet);
+        app.addMapping("/app/*");
+    }
+}
